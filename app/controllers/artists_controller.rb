d95b3171b3bf319ad77e0eda724d628626066ebb
Ayana Bando <ayanaflax@yahoo.com>
2019-02-09 16:14:27 +0000

    Automatically backed up by Learn

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -1,9 +1,12 @@
 class ArtistsController < ApplicationController
   def index
-  end
+    @artists = Artist.all
+  end	  end
 
-  def show
-  end
+
+   def show	  def show
+    @artist = Artist.find(params[:id])
+	 end
 
   def new
     @artist = Artist.new
